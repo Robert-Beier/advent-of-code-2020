3cae0fc4ae3fb1140cecd31699d9df7ee1cb4bae
Robert Beier <r.beier199@gmail.com>
2020-12-01 09:10:35 +0100

    01: Solve part one and part two

---
+++ 01/src/main.rs
@@ -0,0 +1,65 @@
+use std::fs;
+
+#[test]
+fn find_two_summands_should_solve_example_1() {
+    let input = vec![1721, 979, 366, 299, 675, 1456];
+    assert_eq!(find_two_summands(&input, 2020), Some((1721, 299)));
+}
+
+#[test]
+fn find_three_summands_should_solve_example_1() {
+    let input = vec![1721, 979, 366, 299, 675, 1456];
+    assert_eq!(find_three_summands(&input, 2020), Some((979, 366, 675)));
+}
+
+
+fn find_two_summands(potential_summands: &Vec<i32>, sum: i32) -> Option<(i32, i32)> {
+    for a in potential_summands {
+        let b = potential_summands.iter().find(|&&x| x == (sum - a));
+        if let Some(b) = b {
+            return Some((*a, *b));
+        }
+    }
+    None
+}
+
+fn find_three_summands(potential_summands: &Vec<i32>, sum: i32) -> Option<(i32, i32, i32)> {
+    for a in potential_summands {
+        let summands = find_two_summands(potential_summands, sum - a);
+        if let Some(summands) = summands {
+            return Some((*a, summands.0, summands.1));
+        }
+    }
+    None
+}
+
+fn read_input() -> Vec<i32> {
+    let input = fs::read_to_string("input.txt").expect("Failed reading input.txt");
+    input.lines().map(|n| n.parse::<i32>().expect("Failed to parse number.")).collect()
+}
+
+fn part_one() {
+    let input = read_input();
+    let summands = find_two_summands(&input, 2020);
+    if let Some(summands) = summands {
+        println!("Result of part one:\n{}\n", summands.0 * summands.1);
+    } else {
+        println!("No result found for part one.");
+    }
+}
+
+fn part_two() {
+    let input = read_input();
+    let summands = find_three_summands(&input, 2020);
+    if let Some(summands) = summands {
+        println!("Result of part two:\n{}\n", summands.0 * summands.1 * summands.2);
+    } else {
+        println!("No result found for part two.");
+    }
+
+}
+
+fn main() {
+    part_one();
+    part_two();
+}
