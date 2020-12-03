50db5e0669c1417636c57f8814ecdb14e0128f61
Robert Beier <r.beier199@gmail.com>
2020-12-03 20:33:23 +0100

    03: Solve part one

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+use std::time::Instant;
+
+fn part_one(input: String) {
+    let now = Instant::now();
+    let mut x: usize = 0;
+    let mut number_of_trees = 0;
+    let line_length = input.lines().next().map(|line| line.len()).unwrap();
+    for line in input.lines() {
+        if line.chars().nth(x).unwrap() == '#' {
+            number_of_trees += 1;
+        }
+        x = (x + 3) % line_length;
+    }
+    println!("Part one took {} nano seconds", now.elapsed().as_nanos());
+    println!("Result of part one:\n{}\n", number_of_trees);
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Failed reading input.txt");
+    part_one(input);
 }
