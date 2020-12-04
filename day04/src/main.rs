13da14993eebcafe12a47793cd84828be149c393
Robert Beier <r.beier199@gmail.com>
2020-12-04 16:47:54 +0100

    04: Solve part two

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,8 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::{Captures, Regex};
 use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
@@ -39,7 +44,73 @@ fn part_one(passports: &Vec<HashMap<String, String>>) {
     println!("Result of part one:\n{}\n", number_of_valid_passports);
 }
 
+fn part_two(passports: &Vec<HashMap<String, String>>) {
+    let required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    let now = Instant::now();
+    let number_of_valid_passports = passports
+        .iter()
+        .filter(|passport| {
+            required_fields
+                .iter()
+                .filter(|field| {
+                    !passport.contains_key(**field)
+                        || !validate_field(**field, passport.get(**field).unwrap())
+                })
+                .count()
+                == 0
+        })
+        .count();
+    println!("Part two took {} nano seconds", now.elapsed().as_nanos());
+    println!("Result of part two:\n{}\n", number_of_valid_passports);
+}
+
+fn validate_field(field: &str, value: &str) -> bool {
+    lazy_static! {
+        static ref FOUR_DIGITS: Regex = Regex::new(r"^\d{4}$").unwrap();
+        static ref NINE_DIGITS: Regex = Regex::new(r"^\d{9}$").unwrap();
+        static ref COLOR: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+        static ref HEIGHT: Regex = Regex::new(r"^(\d+)(cm|in)$").unwrap();
+    }
+    match field {
+        "byr" => {
+            FOUR_DIGITS.is_match(value) && {
+                let year: usize = value.parse().unwrap();
+                year >= 1920 && year <= 2002
+            }
+        }
+        "iyr" => {
+            FOUR_DIGITS.is_match(value) && {
+                let year: usize = value.parse().unwrap();
+                year >= 2010 && year <= 2020
+            }
+        }
+        "eyr" => {
+            FOUR_DIGITS.is_match(value) && {
+                let year: usize = value.parse().unwrap();
+                year >= 2020 && year <= 2030
+            }
+        }
+        "hgt" => match HEIGHT.captures(value) as Option<Captures> {
+            Some(height_with_unit) => {
+                let height: usize = height_with_unit.get(1).unwrap().as_str().parse().unwrap();
+                let unit = height_with_unit.get(2).unwrap().as_str();
+                match unit {
+                    "cm" => height >= 150 && height <= 193,
+                    "in" => height >= 59 && height <= 76,
+                    _ => false,
+                }
+            }
+            None => false,
+        },
+        "hcl" => COLOR.is_match(value),
+        "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&value),
+        "pid" => NINE_DIGITS.is_match(value),
+        _ => panic!(),
+    }
+}
+
 fn main() {
     let passports = read_input();
-    part_one(&passports)
+    part_one(&passports);
+    part_two(&passports);
 }
