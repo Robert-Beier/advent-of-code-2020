42ec000e60f0f0f46062fc9e2e43a9af81adc3b9
Robert Beier <r.beier199@gmail.com>
2020-12-09 08:13:14 +0100

    07: Parse inner bags

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,11 +1,34 @@
-use regex::Regex;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::{Captures, Regex};
 use std::fs;
 
 #[derive(Debug)]
 struct Rule {
     outer_bag: String,
-    inner_bags: String,
-    // inner_bags: Vec<(String, usize)>,
+    inner_bags: Vec<(String, usize)>,
+}
+
+fn parse_inner_bags(inner_bags: &str) -> Vec<(String, usize)> {
+    lazy_static! {
+        static ref INNER_BAGS: Regex =
+            Regex::new(r"^(?P<amount>\d+) (?P<name>[a-z ]+) bags?$").unwrap();
+    }
+    if inner_bags == "no other bags" {
+        return vec![];
+    }
+    inner_bags
+        .split(", ")
+        .map(|inner_bag| INNER_BAGS.captures(inner_bag).unwrap() as Captures)
+        .map(|inner_bag| {
+            (
+                inner_bag.name("name").unwrap().as_str().to_string(),
+                inner_bag.name("amount").unwrap().as_str().parse().unwrap(),
+            )
+        })
+        .collect()
 }
 
 fn create_rules_list(input: &String) -> Vec<Rule> {
@@ -17,7 +40,7 @@ fn create_rules_list(input: &String) -> Vec<Rule> {
         .map(|line| rule.captures(line).unwrap())
         .map(|captures| Rule {
             outer_bag: captures.name("outer_bag").unwrap().as_str().to_string(),
-            inner_bags: captures.name("inner_bags").unwrap().as_str().to_string(),
+            inner_bags: parse_inner_bags(captures.name("inner_bags").unwrap().as_str()),
         })
         .collect()
 }
