3e699c597bdc7801acc0e6286c03775f495f1a4f
Robert Beier <r.beier199@gmail.com>
2020-12-14 21:21:33 +0100

    13: Read input for part two

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -10,25 +10,25 @@ fn get_wait_time_should_work_for_max_time() {
     assert_eq!(get_wait_time(21, 10), 9);
 }
 
-fn get_wait_time(earliest_start: usize, bus: usize) -> usize {
+fn get_wait_time(earliest_start: u64, bus: u64) -> u64 {
     bus - ((earliest_start - 1) % bus) - 1
 }
 
 #[test]
 fn get_earliest_bus_should_work_for_example1() {
-    let earliest_start = 939usize;
+    let earliest_start = 939u64;
     let buses = vec![7, 13, 59, 31, 19];
     assert_eq!(get_earliest_bus(earliest_start, &buses), 59);
 }
 
-fn get_earliest_bus(earliest_start: usize, buses: &Vec<usize>) -> usize {
+fn get_earliest_bus(earliest_start: u64, buses: &Vec<u64>) -> u64 {
     *buses
         .iter()
         .min_by_key(|&&b| get_wait_time(earliest_start, b))
         .unwrap()
 }
 
-fn parse_input(input: &str) -> (usize, Vec<usize>) {
+fn parse_input(input: &str) -> (u64, Vec<u64>) {
     let lines: Vec<&str> = input.lines().collect();
     (
         lines.get(0).unwrap().parse().unwrap(),
@@ -42,7 +42,19 @@ fn parse_input(input: &str) -> (usize, Vec<usize>) {
     )
 }
 
-fn part_one(earliest_start: usize, buses: &Vec<usize>) {
+fn parse_input2(input: &str) -> Vec<(u64, u64)> {
+    let lines: Vec<&str> = input.lines().collect();
+    lines
+        .get(1)
+        .unwrap()
+        .split(',')
+        .enumerate()
+        .filter(|(_, s)| *s != "x")
+        .map(|(i, n)| (i as u64, n.parse().unwrap()))
+        .collect()
+}
+
+fn part_one(earliest_start: u64, buses: &Vec<u64>) {
     solve("Part one", || {
         let earliest_bus = get_earliest_bus(earliest_start, buses);
         get_wait_time(earliest_start, earliest_bus) * earliest_bus
@@ -53,4 +65,5 @@ fn main() {
     let input = read_input();
     let (earliest_start, buses) = parse_input(&input);
     part_one(earliest_start, &buses);
+    let buses = parse_input2(&input);
 }
