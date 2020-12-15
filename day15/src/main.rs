716a5966a583129addb622a41bd9bd532f76ead0
Robert Beier <r.beier199@gmail.com>
2020-12-15 08:32:58 +0100

    15: Clean up code

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,69 +1,64 @@
 use lib::solve;
 use std::collections::HashMap;
 
+type History = HashMap<usize, Vec<usize>>;
+type StartingNumbers = Vec<usize>;
+
 #[test]
 fn get_nth_number_should_work_for_example_1() {
-    let starting_numbers: Vec<usize> = vec![0, 3, 6];
+    let starting_numbers: StartingNumbers = vec![0, 3, 6];
     assert_eq!(get_nth_number(&starting_numbers, 9), 4);
 }
 
 #[test]
 fn get_nth_number_should_work_for_example_2() {
-    let starting_numbers: Vec<usize> = vec![1, 3, 2];
+    let starting_numbers: StartingNumbers = vec![1, 3, 2];
     assert_eq!(get_nth_number(&starting_numbers, 2020), 1);
 }
 
 #[test]
 fn get_nth_number_should_work_for_example_3() {
-    let starting_numbers: Vec<usize> = vec![2, 1, 3];
+    let starting_numbers: StartingNumbers = vec![2, 1, 3];
     assert_eq!(get_nth_number(&starting_numbers, 2020), 10);
 }
 
 #[test]
 fn get_nth_number_should_work_for_example_4() {
-    let starting_numbers: Vec<usize> = vec![3, 1, 2];
+    let starting_numbers: StartingNumbers = vec![3, 1, 2];
     assert_eq!(get_nth_number(&starting_numbers, 2020), 1836);
 }
 
-fn push_occurrences(
-    number_occurrences: &mut HashMap<usize, Vec<usize>>,
-    number: usize,
-    turn: usize,
-) {
-    let occurrences = number_occurrences.entry(number).or_insert(Vec::new());
-    occurrences.push(turn);
+fn push_turn(history: &mut History, number: usize, turn: usize) {
+    let turns = history.entry(number).or_insert(Vec::new());
+    turns.push(turn);
 }
 
-fn get_next_number(
-    number_occurrences: &HashMap<usize, Vec<usize>>,
-    last_number: usize,
-    turn: usize,
-) -> usize {
-    match number_occurrences.get(&last_number) {
+fn get_next_number(history: &History, last_number: usize, turn: usize) -> usize {
+    match history.get(&last_number) {
         None => 0,
-        Some(occurrences) => turn - occurrences.last().unwrap(),
+        Some(turns) => turn - turns.last().unwrap(),
     }
 }
 
-fn get_nth_number(starting_numbers: &Vec<usize>, n: usize) -> usize {
-    let mut last_number = starting_numbers.last().unwrap().clone();
-    let mut number_occurrences: HashMap<usize, Vec<usize>> = HashMap::new();
-    for (i, starting_number) in starting_numbers
-        .iter()
-        .take(starting_numbers.len() - 1)
-        .enumerate()
-    {
-        push_occurrences(&mut number_occurrences, *starting_number, i + 1);
+fn get_nth_number(starting_numbers: &StartingNumbers, n: usize) -> usize {
+    let mut history: History = HashMap::new();
+    for (i, starting_number) in starting_numbers.iter().enumerate() {
+        push_turn(&mut history, *starting_number, i + 1);
     }
+    let mut next_number = get_next_number(
+        &history,
+        *starting_numbers.last().unwrap(),
+        starting_numbers.len(),
+    );
     for turn in starting_numbers.len() + 1..n {
-        let new_number = get_next_number(&number_occurrences, last_number, turn - 1);
-        push_occurrences(&mut number_occurrences, last_number, turn - 1);
-        last_number = new_number;
+        let current_number = next_number;
+        next_number = get_next_number(&history, current_number, turn);
+        push_turn(&mut history, current_number, turn);
     }
-    get_next_number(&number_occurrences, last_number, n - 1)
+    next_number
 }
 
-fn part_one(starting_numbers: &Vec<usize>) {
+fn part_one(starting_numbers: &StartingNumbers) {
     solve("Part one", || get_nth_number(starting_numbers, 2020));
 }
 
